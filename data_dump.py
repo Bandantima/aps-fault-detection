277e2cfc99f6ed50a51e02e210d4c4f4fc3c8ceb
Bandantima <antimapatel02@gmail.com>
2022-12-04 12:01:24 +0530

    updated

--- data_dump.py
+++ data_dump.py
@@ -8,15 +8,16 @@ DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
 COLLECTION_NAME="sensor"
 
-if __name__=="__main__":
-    df=pd.read_csv(DATA_FILE_PATH)
-    print(f"ROWS AND COLUMNS:{df.shape}")
 
-    #convert dataframe to json so that we can dump these record in mongodb
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    #Convert dataframe to json so that we can dump these record in mongo db
     df.reset_index(drop=True,inplace=True)
 
     json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
 
-   #insert converted json record to mongo db
+    #insert converted json record to mongo db
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
